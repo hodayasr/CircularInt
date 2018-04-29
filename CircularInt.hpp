9249cdc13aa9eef62ea9090cffb7d9825a1a95d7
Hodaya Sror <33117494+hodayasr@users.noreply.github.com>
2018-04-29 17:46:11 +0300

    Update CircularInt.hpp

--- CircularInt.hpp
+++ CircularInt.hpp
@@ -1,30 +1,67 @@
 #include <iostream>
-#include <string>
+using namespace std;
 #pragma once
 
 class CircularInt{
-	public:
-	int num;
-	int first;
-	int last;
-	
-	CircularInt(int x1, int y1);
-	CircularInt operator+ (const CircularInt &circ);
-	friend CircularInt operator+ ( CircularInt &circ, const int &x);
-	friend CircularInt operator+ (const int &x ,const CircularInt &circ);
-	friend CircularInt operator- (const int &x ,const CircularInt &circ);
-	CircularInt operator* (const int &x);
-	CircularInt& operator*= (const int &x);
-	friend CircularInt operator- (const CircularInt &circ);
-	CircularInt operator- (const int &x);
-	friend CircularInt operator/ (const CircularInt &circ, const int &x);
-	CircularInt& operator+= (const int &x);
-	CircularInt &operator++ ();
-	CircularInt operator++ (int);
-	std::ostream &operator<<(std::ostream &ost);
-	friend std::ostream &operator<<(std::ostream &ost , CircularInt &m);
-	friend std::ostream& operator<<(std::ostream& os, CircularInt const& r);
-	void add(int x);
-	void minus(int x);
-	void fix();	
+ 
+    private:
+    int start;
+    int end;
+    int current;
+          
+    public:
+
+        //Constructor
+        CircularInt();
+        CircularInt(int, int);
+        ~CircularInt();
+
+        //Assignment
+        CircularInt& operator =(CircularInt const &);
+        CircularInt& operator =(int);
+
+        //Comparison
+        friend bool operator == (CircularInt const &, int const);
+        friend bool operator == (int const, CircularInt const &);
+        friend bool operator == (CircularInt const &, CircularInt const &);
+        friend bool operator != (CircularInt const &, int const);
+        friend bool operator != (int const, CircularInt const &);
+        friend bool operator != (CircularInt const &, CircularInt const &);
+
+        //Addition
+        CircularInt& operator += (int);
+        CircularInt& operator += (CircularInt const &);
+        friend CircularInt operator + (int, CircularInt const &);
+        friend CircularInt operator + (CircularInt const &, int);
+        friend CircularInt operator + (CircularInt const &, CircularInt const &);
+        CircularInt& operator ++ ();
+        const CircularInt operator ++ (int);
+
+        //Subtraction
+        CircularInt operator - ();
+        CircularInt& operator -= (CircularInt const &);
+        CircularInt& operator -= (int);
+        friend CircularInt operator - (int, CircularInt const &);
+        friend CircularInt operator - (CircularInt const &,int);
+        friend CircularInt operator - (CircularInt const &,CircularInt const &);
+        CircularInt& operator -- ();
+        const CircularInt operator -- (int);
+
+        //Multiplication
+        CircularInt& operator *= (CircularInt const &);
+        CircularInt& operator *= (int);
+        friend CircularInt operator * (int, CircularInt const &);
+        friend CircularInt operator * (CircularInt const &,int);
+        friend CircularInt operator * (CircularInt const &, CircularInt const &);
+
+        //Division
+        CircularInt& operator /= (int);
+        CircularInt& operator /= (CircularInt const &);
+        friend CircularInt operator / (int, CircularInt const &);
+        friend CircularInt operator / (CircularInt const &, int);
+        friend CircularInt operator / (CircularInt const &, CircularInt const &);
+
+        //Stream
+        friend ostream& operator << (ostream &, CircularInt const &);
+        friend istream& operator >> (istream & ,CircularInt &);
 };
