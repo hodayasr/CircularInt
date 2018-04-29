43a5b472c6fe8edd403c7be63165028dade5f3e3
Hodaya Sror <33117494+hodayasr@users.noreply.github.com>
2018-04-29 17:52:47 +0300

    Update CircularInt.cpp

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,251 +1,170 @@
+#include <vector>
+#include <set>
 #include <iostream>
-using namespace std;
-
+#include <string>
 #include "CircularInt.hpp"
 
-//Constructor
-CircularInt::CircularInt(){
-	cout << "No values entered, defining default values 0-10"<< endl;
-	this->start = 0;
-	this->end = 10;
-	this->current=0;
-}
 
-CircularInt::CircularInt(int start, int end){
-	if(end < start ){
-		cout << "Wrong values entered, defining default values 0-10"<< endl;
-		this->start = 0;
-		this->end = 10;
-		this->current=0;
+// בנאי
+CircularInt::CircularInt(int a, int b)
+{
+	if(a > b)
+	{
+		this->max = a;
+		this->min = b;
 	}
-	else{
-		this->start = start;
-		this->end = end;
-		this->current=start;
-	}
-}
-
-CircularInt::~CircularInt(){
-}
-
-int norm(int num, int start, int end){
-	num = num % (end-start+1);
-	if(num>end)
-		num -= (end-start+1);
-	if(num < start)
-		num += (end-start+1);
-	return num;
-}
-
-//Assignment
-CircularInt& CircularInt::operator = (const CircularInt& c){
-	this->start = c.start;
-	this->end = c.start;
-	this->current = c.current;
-	return *this;
-}  
-
-CircularInt& CircularInt::operator = (int num){
-	this->current = norm(num,this->start,this->end);
-	return *this;
-}
-
-//Comparison
-bool operator == (CircularInt const & obj, int const num){
-	return obj.current == num;
-}
-
-bool operator == (int const num, CircularInt const & obj){
-	return num == obj.current;
-}
-
-bool operator == (CircularInt const & a, CircularInt const & b){
-	return a.current == b.current;
-}
-
-bool operator != (CircularInt const & obj, int num){
-	return !(obj.current == num);
-}
-
-bool operator != (int num, CircularInt const & obj){
-	return !(num == obj.current);
-}
-
-bool operator != (CircularInt const & a, CircularInt const & b){
-	return !(a.current == b.current);
-}
-
-//Addition
-CircularInt& CircularInt::operator += (CircularInt const & x){
-	current = norm(current+x.current, start, end);
-	return *this;
-}
-
-CircularInt& CircularInt::operator += (int const x){
-	current = norm(current+x, start, end);
-	return *this;
-}
-
-CircularInt operator + (int add, CircularInt const & x){
-	CircularInt res {x.start, x.end};
-    res.current= norm(add+x.current, x.start, x.end);
-	return res;
-}
-
-CircularInt operator + (CircularInt const & x, int add){
-	return add + x;
-}
-
-CircularInt operator + (CircularInt const & x, CircularInt const & y){
-	CircularInt res {x.start, x.end};
-	res.current = norm(x.current+y.current, x.start, x.end);
-	return res;
-}
-
-CircularInt& CircularInt::operator ++ (){
-	*this += 1;
-	return *this;
-}
-
-const CircularInt CircularInt::operator ++ (int	){
-	CircularInt res {start, end};
-	res.current = current;
-	current = norm(current + 1, start, end);
-	return res;
-}
-
-//Subtraction
-CircularInt& CircularInt::operator -= (CircularInt const & x){
-	current = norm(current-x.current, start, end);
-	return *this;
-}
-
-CircularInt& CircularInt::operator -= (int const x){
-	current = norm(current-x, start, end);
-	return *this;
-}
-
-CircularInt operator - (int num, CircularInt const & x){
-	CircularInt res {x.start, x.end};
-	res.current = norm(res.current-num, res.start, res.end);
-	return res;
-}
-
-CircularInt operator - (CircularInt const & x, int sub){
-	return (sub - x);
-}
-
-CircularInt operator - (CircularInt const & x, CircularInt const & y){
-	CircularInt res {x.start, x.end};
-	res.current = norm(x.current-y.current, x.start, x.end);
-	return res;
-}
-
-CircularInt CircularInt::operator - (){
-	CircularInt res {start, end};
-	res.current = current;
-	res.current -= end;
-	res.current *= -1;
-	return res;
-}
-
-CircularInt& CircularInt::operator -- (){
-	current -= 1;
-	return *this;
-}
-
-const CircularInt CircularInt::operator -- (int){
-	CircularInt res {start, end};
-	res.current = current;
-	current = norm(current - 1, start, end);
-	return res;
-}
-
-//Multiplication
-CircularInt& CircularInt::operator *= (CircularInt const & x){
-	current = norm(current*x.current, start, end);
-	return *this;
-}
-
-CircularInt& CircularInt::operator *= (int const num){
-	current = norm(current*num, start, end);
-	return *this;
-}
-
-CircularInt operator * (int num, CircularInt const & x){
-	CircularInt res {x.start, x.end};
-	res.current = norm(x.current*num, x.start, x.end);
-	return res;
-}
-
-CircularInt operator * (CircularInt const & x, int num){
-	return num * x ;
-}
-
-CircularInt operator * (CircularInt const & x, CircularInt const & y){
-	CircularInt res {x.start, x.end};
-	res.current = norm(x.current*y.current, x.start, x.end);
-	return res;
-}
-
-//Division
-CircularInt& CircularInt::operator /= (CircularInt const & x){
-	current /= x.current;
-	current = current % end;
-	if(current < start)
-		current += end;
-	return *this;
-}
-
-CircularInt& CircularInt::operator /= (int const num){
-	current /= num;
-	current = current % end;
-	if(current < start)
-		current += end;
-	return *this;
-}
-
-CircularInt operator / ( CircularInt const & x, int num){
-	CircularInt res {x.start, x.end};
-	res.current = x.current;
-	if(res.current % num == 0)
-		res /= num;
 	else
-		throw "\"There is no number x in {"+to_string(x.start)+","+to_string(x.end)+"} such that x*"+to_string(num)+"="+to_string(x.current)+"\" ";
-	return res;
-}
-
-CircularInt operator / (int x, CircularInt const & obj){
-	CircularInt res {obj.start, obj.end};
-	x = x % obj.end;
-	res.current = x;
-	if(res.current % obj.current == 0)
-		res /= obj;
-	else
-		throw "\"There is no number x in {"+to_string(obj.start)+","+to_string(obj.end)+"} such that x*"+to_string(x)+"="+to_string(obj.current)+"\" ";
-	return res;
-}
-
-CircularInt operator / (CircularInt const & x, CircularInt const & y){
-	if(x.end != y.end || x.start != y.start)
-		throw "The objects have different cycles";
-	else {
-		CircularInt res {x.start, x.end};
-        if(x.current % y.current == 0)
-            res.current = x.current / y.current;
-        else
-            throw "\"There is no number x in {"+to_string(x.start)+","+to_string(x.end)+"} such that x*"+to_string(x.current)+"="+to_string(y.current)+"\" ";
-		return res;
+	{
+		this->min = a;
+		this->max = b;
 	}
+	this->now = this->min;
+}
+//בנאי מעתיק
+CircularInt::CircularInt(const CircularInt& h)
+{
+	this->min = h.min;
+	this->max = h.max;
+	this->now = h.now;
 }
 
-//Stream
-ostream& operator << (ostream& os, CircularInt const & c){
-	return os << c.current;
+// חילוק והשמה
+CircularInt& CircularInt::operator/=(const int num){
+	*this= *this/num;
+		return *this;
+}
+CircularInt& CircularInt::operator/=(const CircularInt& h){
+	*this= *this/h.now;
+	return *this;
+}
+//חיסור והשמה
+CircularInt& CircularInt::operator-=(const int num){
+	this->now = this->now - num;
+	if(this->now < min)
+	{
+		this->now = this->now % max + max;
+	}
+	return *this;
 }
 
-istream& operator >> (istream & is, CircularInt& x){
-		cin >> x.start >> x.end;
-		x.current = x.start;
-		return is;
+CircularInt& CircularInt::operator-=(const CircularInt& h){
+	this->now = this->now - h.now;
+	if(this->now < min)
+	{
+		this->now = this->now % max + max;
+	}
+	return *this;
 }
+// חיבור והשמה
+CircularInt& CircularInt::operator+=(const int num) {
+	this->now = this->now+num;
+	if(this->now > max)
+	{
+		this->now = this->now % max;
+	}
+	return *this;
+}
+CircularInt& CircularInt::operator+=(const CircularInt& h) {
+	this->now = this->now+h.now;
+	if(this->now > max)
+	{
+		this->now = this->now % max;
+	}
+	return *this;
+}
+
+// הפחתה
+CircularInt CircularInt::operator--(int){
+	CircularInt temp(*this);
+	operator--();
+	return temp;
+}
+CircularInt& CircularInt::operator--(){
+	now--;
+	while(now<min){
+		now=now+max;
+	}
+	return *this;
+}
+
+//הוספה
+CircularInt CircularInt::operator++(int){
+	CircularInt temp(*this);
+	operator++();
+	return temp;
+}
+
+CircularInt& CircularInt::operator++(){
+	now++;
+	if(now > max)
+	{
+		now =now % max;
+	}
+	return *this;
+}
+
+
+
+// כפל והשמה 
+CircularInt& CircularInt::operator*=(const int num){
+	now*=num;
+
+	while(now > max)
+	{
+		now-=max;
+	}
+	while(now < min)
+	{
+		now+=max;
+	}
+
+	return *this;
+}
+
+
+
+CircularInt& CircularInt::operator*=(const CircularInt& h){
+	now*=h.now;
+
+	while(now > max)
+	{
+		now-=max;
+	}
+	while(now < min)
+	{
+		now+=max;
+	}
+
+	return *this;
+}
+
+
+
+//השמה
+CircularInt& CircularInt::operator=(const CircularInt& h){
+	now = h.now;
+	if(now > max)
+	{
+		now =now % max;
+	}
+	while(now<min){
+		now+=max;
+	}
+	return *this;
+
+}
+
+CircularInt& CircularInt::operator=(const int num){
+
+	now = num;
+	if(now > max)
+	{
+		now =now % max;
+	}
+	while(now<min){
+		now+=max;
+	}
+	return *this;
+}
+
+
